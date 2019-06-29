85ce877e9abe4c32d84c1df986be01a7accf25f2
Graushf <gustavo.raush@limitlessgamesgroup.com>
2019-06-29 13:43:44 +0200

    Adding LuaPlus third party library

---
+++ Source/EngineCore/LUAScripting/LuaStateManager.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//========================================================================
+// LuaStateManager.h : Manages the state of the Lua scripting system
+//========================================================================
+
+#include "LuaPlus.h"
+
