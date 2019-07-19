112c65dd84025b9a1e4e1aefee1b8c2348d84bec
GustavoRaush <gustavo.raush@limitlessgamesgroup.com>
2019-07-19 11:58:25 +0200

    Adding MemoryPool

---
+++ Source/EngineCore/AI/Pathing.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// ================================================================
+// Pathing.h : Implements a simple pathing system using A*
+// ================================================================
+
+#include "../Memory/MemoryPool.h"
+#include "../Memory/MemoryMacros.h"
+
